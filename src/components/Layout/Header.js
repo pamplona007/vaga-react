2f631bdb469c26c4dc18ce320c5950136b492fca
Lucas Pamplona <lucas250598@gmail.com>
2020-12-08 16:14:43 -0300

    Addition of AntDesign and start of design system studys

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Row, Col } from 'antd';
+
+const Header = () => {
+    return (
+        <Row>
+            <Col span={6}>
+            </Col>
+            <Col span={18}>
+            
+            </Col>
+        </Row>
+    )
+}
+
+export default Header
