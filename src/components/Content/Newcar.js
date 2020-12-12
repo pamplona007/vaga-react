425c190f7dd560c89390d2a755b2b19b50791673
Lucas Pamplona <lucas250598@gmail.com>
2020-12-12 19:59:46 -0300

    fixed an issue where a deleted image was not really deleted

--- src/components/Content/Newcar.js
+++ src/components/Content/Newcar.js
@@ -1,40 +1,20 @@
-import { Form, Button, Divider, Input, Upload, message } from 'antd'
 import React from 'react'
-import { db } from '../../util/firebaseUtils'
 import firebase from 'firebase';
+import { Modal, Form, Button, Divider, Input, Upload, message, Row, Col, Spin } from 'antd'
+import { db } from '../../util/firebaseUtils'
+import { v4 as uuidv4 } from 'uuid';
+import { useNavigate } from 'react-router-dom';
+
+const { confirm } = Modal;
 
 
 const Newcar = () => {
 
-    const [ loading, setLoad ] = React.useState(false);
+    const [ loading, setLoading ] = React.useState(false);
     const [ imageUrl, setimageUrl ] = React.useState('');
     const [ files, setFiles ] = React.useState([]);
-
-    function getBase64(file) {
-        return new Promise((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
-            reader.onload = () => resolve(reader.result);
-            reader.onerror = error => reject(error);
-        });
-    }
+    const navigate = useNavigate();
             
-    const customUpload = ({ onError, onSuccess, file }) => {
-        const storage = firebase.storage();
-        const metadata = {
-            contentType: 'image/jpeg'
-        }
-        const storageRef = storage.ref();
-        const imageName = Math.floor(Math.random() * 1000);
-        const imgFile = storageRef.child(`images/${imageName}.png`);
-        try {
-            const image = imgFile.put(file, metadata);
-            onSuccess(null, image);
-        } catch(e) {
-            onError(e);
-        }
-    };
-
     const uploadButton = (
         <div>
             <div className="ant-upload-text">Upload</div>
@@ -43,87 +23,112 @@ const Newcar = () => {
 
     
     const onFinish = values => {
-        console.log(values);
+        setLoading(true)
+        values.images = [];
+        values.created = firebase.firestore.FieldValue.serverTimestamp();
         files.forEach(file => {
-            const storage = firebase.storage();
-            const metadata = {
-                contentType: 'image/jpeg'
-            }
-            const storageRef = storage.ref();
-            const imageName = Math.floor(Math.random() * 10000000);
+            const storageRef = firebase.storage().ref();
+            const imageName = `${values.brand}-${values.model}-${uuidv4()}`;
             const imgFile = storageRef.child(`images/${imageName}.png`);
             try {
-                const image = imgFile.put(file, metadata);
+                const image = imgFile.put(file);
+                values.images.push(imageName);
             } catch(e) {
             }
         });      
         db.collection('cars').add(values)
-            .then((response) => console.log(response))
-            .catch((error) => console.log(error))
+            .then(response => {
+                setLoading(false);
+                navigate('/app');
+            })
+            .catch((error) => {
+                setLoading(false);
+                message.error('Houve um erro, tente novamente mais tarde');
+                console.log(error);
+            })
     };
     
     const onFinishFailed = errorInfo => {
         console.log('Failed:', errorInfo);
     };
 
-    let storageRef = firebase.storage().ref();
-    console.log(storageRef);
-
 
     return (
         <>
-            <h1>Novo Carro</h1>
+            <Row>
+                <Col>
+                    <h1>Novo Carro</h1>
+                </Col>
+            </Row>
             <Divider />
-            <Upload
-                name="car"
-                listType="picture-card"
-                className="car-uploader"
-                beforeUpload={file => {
-                    setFiles(() => [...files, file]);
-                    return false;            
-                }}
-                // onChange={handleChange}
-                // customRequest={customUpload}
-                >
-                {imageUrl ? <img src={imageUrl} alt="car" /> : uploadButton}
-            </Upload>
-            <Divider />
-            <Form name="newCarForm" onFinish={onFinish} onFinishFailed={onFinishFailed}>
-                <Form.Item label="Marca" name="brand" rules={[{ required: true }]}>
-                    <Input placeholder="Qual a marca do carro?" />
-                </Form.Item>
-                <Form.Item label="Modelo" name="model" rules={[{ required: true }]}>
-                    <Input placeholder="E qual o modelo?" />
-                </Form.Item>
-                <Form.Item label="Ano" name="year" rules={[{ required: true }]}>
-                    <Input placeholder="Em que ano ele foi lançado?" />
-                </Form.Item>
-                <Divider />
-                <Form.Item label="Cor" name="color" rules={[{ required: true }]}>
-                    <Input placeholder="Qual a cor do carro?" />
-                </Form.Item>
-                <Form.Item label="Placa" name="plate" rules={[{ required: true }]}>
-                    <Input placeholder="Qual a placa do carro?" />
-                </Form.Item>
-                <Form.Item label="Cidade" name="city" rules={[{ required: true }]}>
-                    <Input placeholder="Em que cidade ele foi emplacado?" />
-                </Form.Item>
-                <Divider />
-                <Form.Item label="Quilometragem" name="distance" rules={[{ required: true }]}>
-                    <Input placeholder="Quantos quimômetros o carro possui?" />
-                </Form.Item>
-                <Form.Item label="Data de cadastro" name="register-date" rules={[{ required: true }]}>
-                    <Input placeholder="Data de cadastro no sistema, tornar automático" />
-                </Form.Item>
-                <Form.Item label="Preço" name="price" rules={[{ required: true }]}>
-                    <Input placeholder="Qual o preço que deste carro?" />
-                </Form.Item>
-                <Form.Item>
-                    <Button type="primary" htmlType="submit">
-                        Enviar
-                    </Button>
-                </Form.Item>
-            </Form>
+            <Row>
+                <Col span={24}>
+                    <Spin spinning={loading}>
+                        <Upload
+                            name="car"
+                            listType="picture-card"
+                            className="car-uploader"
+                            beforeUpload={file => {
+                                setFiles(() => [...files, file]);
+                                return false;            
+                            }}
+                            onRemove= {file => {
+                                return new Promise((resolve, reject) => {
+                                    confirm({
+                                        title: 'are you sure to remove this file?',
+                                        onOk: () => {
+                                            let array = files;
+                                            if (array.indexOf(file.originFileObj) > -1) {
+                                                array.splice(array.indexOf(file.originFileObj), 1);
+                                                setFiles(array);
+                                            }
+                                            resolve(true);
+                                        },
+                                    })
+                                })
+                            }}                       
+                            // onChange={handleChange}
+                            // customRequest={customUpload}
+                            >
+                            {imageUrl ? <img src={imageUrl} alt="car" /> : uploadButton}
+                        </Upload>
+                        <Divider />
+                        <Form name="newCarForm" onFinish={onFinish} onFinishFailed={onFinishFailed}>
+                            <Form.Item label="Marca" name="brand" rules={[{ required: true }]}>
+                                <Input placeholder="Qual a marca do carro?" />
+                            </Form.Item>
+                            <Form.Item label="Modelo" name="model" rules={[{ required: true }]}>
+                                <Input placeholder="E qual o modelo?" />
+                            </Form.Item>
+                            <Form.Item label="Ano" name="year" rules={[{ required: true }]}>
+                                <Input placeholder="Em que ano ele foi lançado?" />
+                            </Form.Item>
+                            <Divider />
+                            <Form.Item label="Cor" name="color" rules={[{ required: true }]}>
+                                <Input placeholder="Qual a cor do carro?" />
+                            </Form.Item>
+                            <Form.Item label="Placa" name="plate" rules={[{ required: true }]}>
+                                <Input placeholder="Qual a placa do carro?" />
+                            </Form.Item>
+                            <Form.Item label="Cidade" name="city" rules={[{ required: true }]}>
+                                <Input placeholder="Em que cidade ele foi emplacado?" />
+                            </Form.Item>
+                            <Divider />
+                            <Form.Item label="Quilometragem" name="distance" rules={[{ required: true }]}>
+                                <Input placeholder="Quantos quimômetros o carro possui?" />
+                            </Form.Item>
+                            <Form.Item label="Preço" name="price" rules={[{ required: true }]}>
+                                <Input placeholder="Qual o preço que deste carro?" />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">
+                                    Enviar
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Spin>
+                </Col>
+            </Row>
         </>
     )
 }
